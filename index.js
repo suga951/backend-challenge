b49d3bd14a089059741cd404419b09e0a4347cb9
Agustin Lopez <aalopezba@gmail.com>
2024-10-06 07:39:30 -0300

    chore: cleaned up code

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import { projectRouter } from "./routes/projectRouter.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 const app = express();
+app.use(express.json());
 
-app.get("/api/", (req, res) => {
-  res.send("Hello World");
-});
+app.use("/api", projectRouter());
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
